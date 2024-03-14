9ff35b78e135abe956a5a2ac89b274af476cc2c7
stepheno42 <84373037+stephen042@users.noreply.github.com>
2024-03-14 10:23:02 +0100

    chat

--- resources/views/user-dashboard/includes/script.blade.php
+++ resources/views/user-dashboard/includes/script.blade.php
@@ -1,5 +1,9 @@
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
+<!-- Live Chat widget -->
+<script src="//code.jivosite.com/widget/wsoh58jbyN" async></script>
+<!-- end Live Chat widget -->
+
 <!-- Vendor JS Files -->
 <script src="{{ URL('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
 <script src="{{ URL('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
